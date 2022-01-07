d2a6f321c6508f4a2d566cd55a7c11ab24e9c5d0
raynardj <b2ray2c@gmail.com>
2022-01-07 18:16:09 +0800

    🚀 e2e template

--- test/end_to_end/image_problem.py
+++
@@ -1,9 +0,0 @@
-from tai_chi_engine.utils import df_creator_image_folder
-from pathlib import Path
-
-TEST_DIR = Path(__file__).parent.parent
-DATA = TEST_DIR/"data"/"bear_dataset"
-
-def test_image_classification():
-    df = df_creator_image_folder(DATA)
-    assert True
