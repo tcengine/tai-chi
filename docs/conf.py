43c4deda7cd9e936d85e2181587959b1b30ac99b
raynardj <b2ray2c@gmail.com>
2022-01-08 22:18:16 +0800

    🧊 auto doc

--- docs/conf.py
+++ docs/conf.py
@@ -10,11 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../'))
 # -- Project information -----------------------------------------------------
 
 project = 'Tai-Chi Engine'
@@ -32,6 +30,8 @@ def reading_version():
                 return line.split("=")[1].strip()
 
 # The full version, including alpha/beta/rc tags
+
+
 release = reading_version()
 
 import sphinx_rtd_theme
